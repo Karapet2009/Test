6e6cb02930c8f150b32ef6f5022af8bc89a89f18
Karapet2009 <mkrtchyan.karo@icloud.com>
2026-02-27 19:04:13 +0400

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+numOne = int(input("Write any number: "))
+numTwo = int(input("Write another number: "))
+symbol = input("Write any symbol (+, -, *, /): ")
+
+if symbol == "+":
+    print(numOne + numTwo)
+elif symbol == "-":
+    print(numOne - numTwo)
+elif symbol == "*":
+    print(numOne * numTwo)
+elif symbol == "/":
+    print(numOne / numTwo)
+else:
+    print("You entered the wrong symbol")
